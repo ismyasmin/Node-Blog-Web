d4ccfec2608b890f9fce2c58564725f801d8faff
Yasmin <yasminxismail@gmail.com>
2025-10-12 10:14:09 +0100

    Add routing and html pages, add nav links in index.html about.html

--- app.js
+++ app.js
@@ -8,16 +8,25 @@ const app = express(); // invoking that function to create an instance of Expres
 app.listen(3000); // returns instance of the server
 
 // listen for a get request
-app.get('/', (req, res) => { 
-    // not a good way to send HTML to the browser
-    res.send('<p>home</p>'); // infers the type of content trying to send to the browser & automatically sets the content type header
-                // infers status code too
+// app.get('/', (req, res) => { 
+//     // not a good way to send HTML to the browser
+//     res.send('<p>home</p>'); // infers the type of content trying to send to the browser & automatically sets the content type header
+//                 // infers status code too
 
-}); // two argus: 1: path to url( here will be root of domain ), 2: fire function of request + response object
-// req: info about the request such as url or the method i.e get, post. 
+// }); // two argus: 1: path to url( here will be root of domain ), 2: fire function of request + response object
+// // req: info about the request such as url or the method i.e get, post. 
+
+// app.get('/about', (req, res) => { 
+//     res.send('<p>about</p>'); 
+// })
+
+
+app.get('/', (req, res) => { 
+    res.sendFile('./views/index.html', { root: __dirname});
+});
+
 
 app.get('/about', (req, res) => { 
-    res.send('<p>about</p>'); 
-})
-
+    res.sendFile('./views/about.html', { root: __dirname});
+});
 
